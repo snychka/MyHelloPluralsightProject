c4d12726164d2dd4a1abcc59c2501324b4763675
snychka <stefannychka@gmail.com>
2019-09-23 14:03:14 -0600

    1st test

--- test/tests.js
+++ test/tests.js
@@ -2,24 +2,26 @@ const fs = require('fs');
 const $ = require('jquery');
 const jsdom = require('jsdom');
 const { JSDOM } = jsdom;
-//const assert = require('assert');
+const chai = require('chai')
+const assert = chai.assert;
 
 describe('all module tests', () => {
   describe('module 1 tests', () =>  {
     let dom;
-    before (() => {
+    before ((done) => {
       fs.readFile('./index.html', 'utf8', (err, data) => {
-        console.log('here');
         if (err) { console.log(err); throw err; }
         // todo: what happens if file doesn't exist?
         // maybe have a test?
-        console.log(data);
+        // todo: use jquery
         dom = new JSDOM(data);
-        console.log(dom.window.document.querySelector('head'));
+        done();
       });
     });
-    it('test description', () =>  {
-      //assert.equal([1, 2, 3].indexOf(4), -1);
+    it('title exists in head and contents some text @title-exists-and-is-non-zero', () =>  {
+      let title_dom = dom.window.document.querySelector('head > title');
+      assert(title_dom !== null, 'title tag does not exist');
+      assert(title_dom.textContent.trim().length >= 1, 'title has no text');
     });
   });
 });
